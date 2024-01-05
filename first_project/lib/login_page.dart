4a3f455ed5e36b870b1ec2dcea2a4e1fb3561c17
bruna-anunciacao <bruunaaa04@gmail.com>
2024-01-05 12:53:35 -0300

    estilizando a tela de login

--- first_project/lib/login_page.dart
+++ first_project/lib/login_page.dart
@@ -12,59 +12,87 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String email = '';
   String senha = '';
+  Widget _body() {
+    return SingleChildScrollView(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Container(
+              width: 200,
+              height: 200,
+              child: Image.asset('assets/images/login.png'),
+            ),
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(9.0),
+                child: Column(children: [
+                  TextField(
+                    onChanged: (text) {
+                      email = text;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  Container(height: 20),
+                  TextField(
+                    onChanged: (text) {
+                      senha = text;
+                    },
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Senha',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  Container(height: 20),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.cyan.shade100,
+                        foregroundColor: Colors.black),
+                    onPressed: () {
+                      if (email == 'admin@admin.com' && senha == 'admin') {
+                        Navigator.of(context).pushReplacementNamed('/home');
+                      } else {
+                        print('Login invalido');
+                      }
+                    },
+                    child: Container(
+                        width: double.infinity,
+                        child: Text(
+                          'Entrar',
+                          textAlign: TextAlign.center,
+                        )),
+                  )
+                ]),
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Login'), actions: [CustomSwitch()]),
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: EdgeInsets.all(8.0),
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Container(
-                width: 200,
-                height: 200,
-                child: Image.asset('assets/images/login.png'),
-              ),
-              TextField(
-                onChanged: (text) {
-                  email = text;
-                },
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              Container(height: 20),
-              TextField(
-                onChanged: (text) {
-                  senha = text;
-                },
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Senha',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              Container(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  if (email == 'admin@admin.com' && senha == 'admin') {
-                    Navigator.of(context).pushReplacementNamed('/home');
-                  } else {
-                    print('Login invalido');
-                  }
-                },
-                child: Text('Entrar'),
-              )
-            ]),
-          ),
-        ),
+      body: Stack(
+        children: [
+          SizedBox(
+              height: MediaQuery.of(context).size.height,
+              child: Image.asset(
+                'assets/images/background.png',
+                fit: BoxFit.cover,
+              )),
+          Container(color: Colors.black.withOpacity(0.7)),
+          _body(),
+        ],
       ),
     );
   }
